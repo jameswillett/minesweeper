877e94c738f97b33b5a88893550b540a08226848
James Willett <jms.wlltt@gmail.com>
2019-08-09 23:52:28 -0400

    ok for real one last thing

--- src/App.js
+++ src/App.js
@@ -370,8 +370,8 @@ class App extends Component {
                     onMouseDown={this.suspense}
                     onContextMenu={(e) => this.handleCellRightClick(e, cell)}
                     // onContextMenu={(e) => e.preventDefault()}
-                    // onPointerDown={this.handlePointerDown(cell)}
-                    // onPointerUp={this.handlePointerUp(cell)}
+                    onTouchStart={this.handlePointerDown(cell)}
+                    onTouchEnd={this.handlePointerUp(cell)}
                     // onPointerLeave={this.handlePointerLeave}
                   >
                     {this.state.gameOver && !cell.flagged && cell.isMine && !isLosingCell(cell)
