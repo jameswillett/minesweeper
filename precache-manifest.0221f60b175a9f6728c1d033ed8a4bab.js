a7834ae7ee9d64bb831795b523d5be057e91b800
James Willett <jms.wlltt@gmail.com>
2019-11-21 23:38:48 -0500

    Updates

---
+++ precache-manifest.0221f60b175a9f6728c1d033ed8a4bab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159bcf0ef63f4c82ee907441bd9b9208",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "ff9b1f24e49608546ab3",
+    "url": "/minesweeper/static/css/main.df2fd388.chunk.css"
+  },
+  {
+    "revision": "8f599092a06c5606f336",
+    "url": "/minesweeper/static/js/2.566ebe6d.chunk.js"
+  },
+  {
+    "revision": "ff9b1f24e49608546ab3",
+    "url": "/minesweeper/static/js/main.507adfd8.chunk.js"
+  },
+  {
+    "revision": "0f86e2ffe97a36d3984a",
+    "url": "/minesweeper/static/js/runtime~main.dd26f679.js"
+  }
+]);
